0e20734a36b83fd6c777a7ad5a16435573529358
H. de Vries <hdevries@fastmail.com>
2018-12-04 22:46:17 +0100

    add day two three and four

---
+++ 2/src/Two.kt
@@ -0,0 +1,310 @@
+fun main() {
+
+    val input = """dghfbsyiznoumojleevappwqtr
+dghfbsyiznoumkjljevacpmqer
+dghfbbyizioumkjlxevacpwdtr
+dghfbsesznoumkjlxevacpwqkr
+dghfbsynznoumkjlxziacpwqtr
+cghfbsyiznjumkjlxevacprqtr
+dghfjsyizwoumkjlxevactwqtr
+dghfdsyfinoumkjlxevacpwqtr
+hghfbsyiznoumkjlxivacpwqtj
+dgcfbsyiznoumkjlxevacbuqtr
+dghfbsyiznoymnjlxevacpwvtr
+dfhfbsyiznoumkulxevacptqtr
+dghfasyiznovmkjlxevacpwqnr
+dghfbsyihnouikjlxevackwqtr
+dghfbayiznolmkjlyevacpwqtr
+jghfbsyiznoumnjldevacpwqtr
+dhhfbsyuznoumkjlxevakpwqtr
+nehfrsyiznoumkjlxevacpwqtr
+dghfbsyiznxdmkolxevacpwqtr
+dgpfbsyizwlumkjlxevacpwqtr
+yghfbsyiznoumkjlsevacpwqtm
+dghfssyiznoumkjlxevvcpwqjr
+dahfbsyiznoumkjlfevacpwqto
+duhfcsyiznouvkjlxevacpwqtr
+dghfbvyiznoumkjlrevacpwvtr
+dghfgsyiznoumknlxgvacpwqtr
+jghfbeyiznkumkjlxevacpwqtr
+daofbsyiznoumkjlxevampwqtr
+dghfbsyiznojmkjlxeracpcqtr
+dghnbsyiznouokjlxevaclwqtr
+dgifbsyiznoumkjlxevnspwqtr
+dgkfpsziznoumkjlxevacpwqtr
+dghfxsyijnoumkjlxevaccwqtr
+dghfbsyiznolmkjlwevzcpwqtr
+dkhfbsaiznoumkjlxevacpwqtg
+dghfbsygknoumkjlaevacpwqtr
+dghfbsyizizumkjlxevacpxqtr
+ighfbbyijnoumxjlxevacpwqtr
+dghfbsyizrouekjlxevacpwktr
+dghobsyiznoujkjlxevacnwqtr
+dghpbsyizyoumkjlxeaacpwqtr
+dghffsyiznoymkjlxevacewqtr
+dghkbssiznoumzjlxevacpwqtr
+dghfbsyawnoumkjlxevacpwjtr
+drhfbsyiznormkjlfevacpwqtr
+dghfbsoiznouwkjlxevacpwqtn
+dghfmsyiznoumkjlxlvecpwqtr
+dxhfbsyiznoumkjlxeeacvwqtr
+dghnbsyiznoumkjsxevacpwqur
+dghfbsyiznrujkjlxevacpwqtc
+dghfbstoznoumhjlxevacpwqtr
+dghfboyiznzumkjlvevacpwqtr
+dghfbsyiznjumkjlxevgcpwmtr
+dghfbsnizaoumkjlxevacpwetr
+dghfbsyirnoumkjoxevacplqtr
+dghfbsyiznoumkjlxavvckwqtr
+dghfbjdiznoumkjlxevacpwptr
+dghfbsywznoumkjlxeiacpwqcr
+djhfbsyizuoumkjlxelacpwqtr
+dghffsniznoumkjlxpvacpwqtr
+dghebsyizuoumkjlxevecpwqtr
+rghfbsyiznourkjcxevacpwqtr
+dghfbsyignoumkwlxenacpwqtr
+dghfbsyiznrufkjlxevacpwqth
+dgifbsyiznoumkjlxevacpjqnr
+dghfbsyiznoumkjbxevaxpwqtw
+dsufbsyizncumkjlxevacpwqtr
+dihfbsyiznoumujlxecacpwqtr
+dghfbiyiznoumkjlxevajpwqtn
+dghqbsyixnoumkjlrevacpwqtr
+dghfbsyiznouukjlxeuacpwqtx
+dghfbsyizyoumksfxevacpwqtr
+dghfbsiiznopfkjlxevacpwqtr
+eghfbsyidnoumkjlxexacpwqtr
+dghfbgyiznouwkjlwevacpwqtr
+dghfbsyyznoumkjlxevacwwqtf
+bghfbtypznoumkjlxevacpwqtr
+dghfbsyiznoumtjlxebacpwetr
+dghfbsgiznonmkplxevacpwqtr
+dghfbsyiznoumxjlxevanpwqpr
+dghfbsyiznwumujuxevacpwqtr
+dghxbsyiznoumkjzxevaypwqtr
+dghfbsyhznoumkjlxlvacpiqtr
+dghfbsyiznoumkjlxevzcnwqrr
+dvhfbsyiznoumkjluevacpzqtr
+dghcbsyiznoumkjlxmvacpwetr
+dghfbsyiznohmkjvxbvacpwqtr
+dghfzsyiznouokjlxevacpwqpr
+dghfbsyiznoumkjlxevachtqth
+dghfbsyiznoumkjlxjvacpfutr
+dghfbsyiznoumkjlxevsppwqtt
+dghfusyiznouakhlxevacpwqtr
+dghfbsyizcoumkjlxrvaipwqtr
+dghebsyipnoumfjlxevacpwqtr
+dgdfbsyiznoumkjlwevacpkqtr
+dghfbsyiznoumkjlcffacpwqtr
+dghfbsypznfumkjlxevacpwqar
+dghfbsyiznojmkjlxevgcpkqtr
+dghfbsyiznoumkjlaevlcpwstr
+dgafrsyiunoumkjlxevacpwqtr
+dghfbsyiznouqljlxevacrwqtr
+dyhkbsyiznokmkjlxevacpwqtr
+pghfbsciznoumkjlxevacpwvtr
+dghfbxyiznonmkjllevacpwqtr
+ighfbsyizxoumkjlxevacpzqtr
+dgffbsyoznoumkjlxevacpwqto
+hghfbsyiznoumkjlpevachwqtr
+dlhfosyiznoumkjldevacpwqtr
+dghfbsvizkoumkjlxvvacpwqtr
+dbafbsyiznozmkjlxevacpwqtr
+dghfbsyidnoumkjlxrvjcpwqtr
+dghfbsyiznfumkjlxeqacpwqta
+dghfbsfiznoumkjvxevacjwqtr
+dghfbsyimnoumrjlhevacpwqtr
+dghflsyiznoumkjlxevacpvqmr
+dghfbmfiznoumkjlxevacpdqtr
+dghfbsyizsouzkjlxevscpwqtr
+dghfksyiznoumimlxevacpwqtr
+dghfbsyiznoumkjlxevbwpwqur
+wghcbsyiznoumkjlkevacpwqtr
+kghfbioiznoumkjlxevacpwqtr
+dghfbsiizeoumkjlxmvacpwqtr
+dglfbsyilnoumkjlxevpcpwqtr
+dgqfbsylznoumkjlxevacpwqcr
+dglfhsyiznoumkjlxevacpwqdr
+dghfbsymznoumkjlxbvacpwqtb
+hghfbsyizhoumkjlxtvacpwqtr
+dghdbsyiznoumkjlxeiacpyqtr
+dohfbsyiznoumkjmxlvacpwqtr
+xhhfbsyiznoumkjlxegacpwqtr
+dlhfbsyiznoumkjlxnvahpwqtr
+dghfbsyiznovdpjlxevacpwqtr
+dgcfbsyiznoumkjlxevactwqdr
+dghfksyiknoumkjlxevacpwqcr
+ughfqsyiznoumkjlxevacpwctr
+dghfbjyiznoumkjlxsvacnwqtr
+dgwfbagiznoumkjlxevacpwqtr
+dghfbsyiznoumknlxevtcpwqdr
+jghfksyiznoumkjlxeoacpwqtr
+dghfbsyiznoimkjlwezacpwqtr
+dghfbsyiunoumkjlxeqacpwstr
+dghfbsyizjoumkwlxevaypwqtr
+dghfysriznoumkjlxevucpwqtr
+dghfbsygzjoumkjfxevacpwqtr
+dghfbhviznoumkjlxevacpwqtq
+dghfbsyiznoumkjvwevacpwqur
+dghfbsyiznoumtjlxevacplqnr
+yghfbsysznouykjlxevacpwqtr
+dgwfbsiiznoumkjlxevacfwqtr
+dghfbsyizooumkjlxevampiqtr
+dshfbsyiznoumkjlxevawpoqtr
+dghtbsyxznuumkjlxevacpwqtr
+dkhfblyiznoumkjlxevacpaqtr
+dgkfbsyiinoumkjlxegacpwqtr
+dghfbtxiznouhkjlxevacpwqtr
+dghfbsyiznoumkjlxkvmcpeqtr
+dghfbsyiznoumkjlghvacpwqmr
+dghfbsbizioumkjlcevacpwqtr
+dphfbsyizhoumkjwxevacpwqtr
+dghfbsyiznqumkjlugvacpwqtr
+dghfbsjinnoumkjlxevacpwetr
+mghfbsyiznoumkjlxfvacpjqtr
+dghfbsxiznoumkjlxetacwwqtr
+dghmbsyiznoumbjlxevacpwqyr
+dghfbsyiznwumkjlwevacmwqtr
+dgkfbsyiznotmkjlxevacpwstr
+dghfbsyiznouykjlxeiacuwqtr
+dghfbsynznbhmkjlxevacpwqtr
+dgyfbsyiznoumtjlbevacpwqtr
+dghfbftiznoumkjlxevacpwatr
+dghfvsyiznouikjlievacpwqtr
+dghfbsyiznodmkjlxevncpwqtz
+yfhfbsyiznoumkjluevacpwqtr
+dghfbzyiznoumhflxevacpwqtr
+dphfbsyizncumkjlxevacpwqtf
+dghfasyiznoumkjlxeaicpwqtr
+dgffbsyiznoumkjlzevacpwqsr
+dghfbsyiznoumkmxxcvacpwqtr
+dghffsyiznoumkjlxevacpwqre
+dghfbsyizndmmkjlxemacpwqtr
+dghfbsviznoamkjlxevappwqtr
+dghfbsyiznouckrlxevacpdqtr
+dgwfbsyiznyumkjlxevacpqqtr
+dujfbsyiznoumgjlxevacpwqtr
+dghobsailnoumkjlxevacpwqtr
+dghfkqyiznoumknlxevacpwqtr
+dghfbyypznoumkjlxevacpwatr
+wqhfbsyiznoumkjlxevzcpwqtr
+dghfbsyiznoumwjlxrvacppqtr
+dghfbsymznoumkflxevacplqtr
+dghfbsyiznounkjpgevacpwqtr
+ighfbsyijnoumxjlxevacpwqtr
+dghfbsyizroumkjllevncpwqtr
+dghfbsliznokmkjlxevacpwqtb
+dgefbsyiznoumkqlxevpcpwqtr
+dghfbtypznouzkjlxevacpwqtr
+dmhfbsyiznoumkjlxeyactwqtr
+vohfbsyiznoumkjlqevacpwqtr
+dgsfpsyiznodmkjlxevacpwqtr
+dghfzsyijnoumkjnxevacpwqtr
+dghfbayijroumkjlxevacpwqtr
+dghfbsyiznodmxjlxgvacpwqtr
+dghfbsyiznocmkjlxhvaipwqtr
+dghmbsyignoumkjlxevacpoqtr
+dghfbsyiznosmkjlncvacpwqtr
+dggfbsyiznuumkjlxevacpwqrr
+dghibsyilnoumkjlxevacowqtr
+dghfbsyiznoumkjluevbcowqtr
+dghfbsaiznyuvkjlxevacpwqtr
+dgnfxsyiznommkjlxevacpwqtr
+dghfbnyiznoumkjlsnvacpwqtr
+dghfssiiznoumkjlxavacpwqtr
+dghfbsyizneumajlxevacfwqtr
+dghfbsyiznoumkjlxevycpvptr
+qghfbsyizgoumkjlxevacpwttr
+vghfbsyiznoumkjlievaepwqtr
+dghfbsyiznoumejlxjvacpwdtr
+dghfbsyispoumkjlxevacpwqtg
+duhfbsyizpoumkjlxenacpwqtr
+dghfbsyifnoumkblxevacpnqtr
+bghfbsyxznoumkjleevacpwqtr
+dgtfbsyzpnoumkjlxevacpwqtr
+dghfbsyiznoumkjlsecacpwqth
+dghfqsyiznjumkjlxevawpwqtr
+dgcfbsyizboumkjlxevacqwqtr
+dghfbqyiznoumkjkxevacpwqtj
+dgyfbsyfznoumkjlievacpwqtr
+dghfdsyiznoumkplxevacpwdtr
+dphfbsyuznkumkjlxevacpwqtr
+dghfbsyiznoupkjitevacpwqtr
+dghfisyiznoamkjlxevacpwqwr
+dgufbsyiznoumkjlxivvcpwqtr
+dghfbvyiznoumkjlxevacvwqtz
+dghfbsyiqnxumkjlxbvacpwqtr
+dghubsyiznqumkflxevacpwqtr
+dghfbsyiznzumkjlxevacpdbtr
+dghfbsyiznoumkjlxehacpwwrr
+mghfbsyiznoumkjlxevacpwqbp
+dvhfbryiznoumkclxevacpwqtr
+dghbbsyiznotmkjlxevacpwqhr
+dghfrsyiznoomkjlxevacpwqto
+dghfbkyiznoumkjlxeracpxqtr
+dghfbfyizfoumkjlxevacpwjtr
+dghfbsyizqoulkjlxevacpwqtt
+dghfbsyiwnoumkjlxevacxwgtr
+dghfbsyiznormkjlgxvacpwqtr
+dghybsyizioumkjoxevacpwqtr
+dchfbsyiznoumkjlxyvacpwqtc
+dgyfbsyiznouckjlxewacpwqtr
+dakfbsyeznoumkjlxevacpwqtr"""
+
+    val inputList = input.split("\n")
+
+    println("-- first star --")
+    var twos = 0
+    var threes = 0
+
+    inputList.forEach { line ->
+        var twosDone = false
+        var threesDone = false
+
+        line.forEach { letter ->
+            if (line.count { it == letter } == 2 && !twosDone) {
+                twos++
+                twosDone = true
+            }
+            if (line.count { it == letter } == 3 && !threesDone) {
+                threes++
+                threesDone = true
+            }
+        }
+    }
+
+    val solution1 = twos * threes
+    println(solution1)
+
+//    val testInput = "dgyfbsyiznouckjlxewacpwqtr\n" +
+//            "dakfbsyeznoumkjlxevacpwqtr"
+    val testInput = "abcd\n" +
+            "abcf"
+    val testInputList = testInput.split("\n")
+
+    println("-- second star --")
+    testInputList.forEach { line ->
+        run {
+            testInputList.forEach { otherLine ->
+                println("---Next line")
+
+                val zipped = (line zip otherLine)
+                zipped.forEach { tuple ->
+                    println(tuple)
+                    println(tuple.first)
+                    println(tuple.second)
+
+                    if (tuple.first != tuple.second) {
+                        if (tuple.first != tuple.second) {
+                            println("not it")
+                            return@forEach
+                        }
+                        println("this is the string: ${line}")
+                    }
+                }
+            }
+        }
+    }
+
+
+}
